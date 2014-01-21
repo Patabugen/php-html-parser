bf15faf3ddd954d3cbb49bd469c5bd05ee62b81d
andig <cpuidle@gmx.de>
2014-01-21 13:05:05 +0100

    Fixed nesting test

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -22,16 +22,17 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 		    <div class="main1">.main1</div>
 		    <div class="main2">div.main2</div>
 		    <div class="main3"><h1></h1><h2>.main3 h2</h2></div>
+		    <div><p><span>div p span</span></p></div>
 		    <div><h3>div>h3</h3></div>
 		    <h1></h1><div>h1+div</div>
-		    <div id="selectorid">#selectorid</div>
+		    <div id="id1">#id1</div>
+		    <div id="id2">div#id2</div>
 		    <div class="text">.text:text</div>
-		    <div>div:contains(containsmain)</div>
+		    <div>div:contains(something)</div>
 		    <div class="attr">div[class=attr]</div>
 		    <div><a href="#">a[href="#"]</a></div>
 		    <div><a title="first episode">a[title~=episode]</a></div>
 		    <div><a alt="first episode">a[alt]</a></div>
-		    <div><p><div>div p div</div></p></div>
 		<!--
 		    <div><p>p:first</p><p>p:second</p></div>
 		    <div>p:has(strong)</div>
@@ -47,7 +48,7 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $harness = Parser::from_string(self::$selectors);
         $parser = Parser::from_string(self::$selectors);
 
-        foreach ($harness->find('div') as $n) {
+        foreach ($harness->find('body div') as $n) {
             $selector = trim($n->text);
             echo $selector."\n";
             $n = $parser->find($selector, 0);
